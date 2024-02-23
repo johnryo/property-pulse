f7c482949e17d659f2327940da6d9a4cb34c6856
johnryo <jpogge55@gmail.com>
2024-02-22 18:56:13 -0800

    Create and implement custom Not Found and loading spinner pages

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,6 @@
 import Hero from '@/components/Hero';
 import InfoBoxes from '@/components/InfoBoxes';
 import HomeProperties from '@/components/HomeProperties';
-import Footer from '@/components/Footer';
 
 const HomePage = () => {
   return (
