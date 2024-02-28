7a6930eeddd1766496279d1c4ed01c395612431f
johnryo <jpogge55@gmail.com>
2024-02-28 15:30:29 -0800

    Corrections to propertyData object

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -20,13 +20,11 @@ export const authOptions = {
   callbacks: {
     // Invoked on successful sign-in
     async signIn({ profile }) {
-      // 1. Connect to db
       await connectDB();
-      // 2. Check if user exists
+
       const userExists = await User.findOne({ email: profile.email });
-      // 3. If not, then add user to db
+
       if (!userExists) {
-        // Truncate user name if too long
         const username = profile.name.slice(0, 20);
 
         await User.create({
@@ -35,16 +33,16 @@ export const authOptions = {
           image: profile.picture,
         });
       }
-      // 4. Return true to allow sign-in
+
       return true;
     },
+
     // Modifies the session object
     async session({ session }) {
-      // 1. Get user from db
       const user = await User.findOne({ email: session.user.email });
-      // 2. Assign the user id to the session
+
       session.user.id = user._id.toString();
-      // 3. Return session
+
       return session;
     },
   },
