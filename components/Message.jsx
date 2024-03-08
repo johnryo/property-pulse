f7a7477ae18fd2d0bed2ab8bd9e284f039a6787b
johnryo <jpogge55@gmail.com>
2024-03-08 15:16:08 -0800

    Finish basic message count display: implement global context; make message count update immediately

--- components/Message.jsx
+++ components/Message.jsx
@@ -2,13 +2,13 @@
 
 import { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
-// import { useGlobalContext } from '@/context/GlobalContext';
+import { useGlobalContext } from '@/context/GlobalContext';
 
 const Message = ({ message }) => {
   const [isRead, setIsRead] = useState(message.read);
   const [isDeleted, setIsDeleted] = useState(false);
 
-  // const { setUnreadCount } = useGlobalContext();
+  const { setUnreadCount } = useGlobalContext();
 
   const handleReadClick = async () => {
     try {
@@ -18,7 +18,7 @@ const Message = ({ message }) => {
       if (res.status === 200) {
         const { read } = await res.json();
         setIsRead(read);
-        // setUnreadCount((prevCount) => (read ? prevCount - 1 : prevCount + 1));
+        setUnreadCount((prevCount) => (read ? prevCount - 1 : prevCount + 1));
         if (read) {
           toast.success('Marked as read');
         } else {
@@ -39,7 +39,7 @@ const Message = ({ message }) => {
 
       if (res.status === 200) {
         setIsDeleted(true);
-        // setUnreadCount((prevCount) => prevCount - 1);
+        setUnreadCount((prevCount) => prevCount - 1);
         toast.success('Message Deleted');
       }
     } catch (error) {
